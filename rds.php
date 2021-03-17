d37dfa9ef163a5e7a2fc709a59e2a5c0bdd3fe01
vikas99341 <48900898+vikas99341@users.noreply.github.com>
2021-03-17 08:03:34 +0530

    updated rds.php

--- rds.php
+++ rds.php
@@ -7,7 +7,7 @@
 <?php
 
 // Setup RDS connection details - RDS_HOSTNAME, DB_USERNAME and DB_PASSWORD as Env Variables in Elastic Beanstalk or OpsWorks
-// E.g. Set Env variable RDS_HOSTNAME to myrds.cle1covvccqa.us-east-1.rds.amazonaws.com:3306
+// E.g. pop Set Env variable RDS_HOSTNAME to myrds.cle1covvccqa.us-east-1.rds.amazonaws.com:3306
  
 $con=mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD']);
 
